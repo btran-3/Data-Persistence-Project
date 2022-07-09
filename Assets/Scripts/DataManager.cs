76b7c7d51f114e43fb037ebbe73978a641c505df
bt <britra832@gmail.com>
2022-07-09 14:54:19 -0400

    creating initial scripts and importing TMPro

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    public static DataManager instance;
+
+    public string playerName;
+    public int highScore;
+    
+
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MenuManager : MonoBehaviour
+{
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
